547ea3ea69cb13adc8250ce10cd904122c13f476
Yongveloper <aaa9015@naver.com>
2021-12-02 22:29:54 +0900

    exprss설치 및 타입정의파일 @types/express 설치

--- dist/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-console.log('Hello world!!');
-console.log('Hi!jehehehehe');
-for (let i = 0; i < 100; i++) {
-    console.log(i);
-}
-//# sourceMappingURL=main.js.map
--- src/main.ts
+++
@@ -1,5 +0,0 @@
-console.log('Hello world!!');
-console.log('Hi!jehehehehe');
-for (let i = 0; i < 100; i++) {
-  console.log(i);
-}
