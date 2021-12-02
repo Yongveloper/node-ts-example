8c419dfbfaf6087b07e6b60dafe7d7af59e483a1
Yongveloper <aaa9015@naver.com>
2021-12-02 22:19:29 +0900

    디버깅 사용 sourceMap true

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,7 @@
 "use strict";
 console.log('Hello world!!');
 console.log('Hi!jehehehehe');
+for (let i = 0; i < 100; i++) {
+    console.log(i);
+}
+//# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,5 @@
 console.log('Hello world!!');
 console.log('Hi!jehehehehe');
+for (let i = 0; i < 100; i++) {
+  console.log(i);
+}
